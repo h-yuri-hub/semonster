43ffacc4261f638572c55d9081cf99357b63ef5f
hyuri <m1m21a35@oit.ac.jp>
2021-06-28 10:31:09 +0900

    Main.javaとMonster.javaの変更

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello SEMonster");
+    Monster testMonster = new Monster();
+    System.out.println(testMonster.toString());
   }
 }
